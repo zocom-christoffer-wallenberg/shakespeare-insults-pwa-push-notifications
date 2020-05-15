132cc74b54a658fcdf34af1857e9a5c15b652f7b
Christoffer Wallenberg <christoffer.wallenberg@zocom.se>
2020-05-15 14:52:27 +0200

    Added code

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ import push from './push-notifications.js';
 
 const insultElem = document.querySelector('.insult');
 const playElem = document.querySelector('.play');
+let stream = {};
 
 function showInsult(insultObj) {
 	insultElem.innerHTML = insultObj.insult;
@@ -28,6 +29,53 @@ document.addEventListener('touchstart', event => {
 })
 
 getInsult();
+
+function randomiseHue(videoElem) {
+  let i = 0;
+  let interval = setInterval(() => {
+    let number = Math.floor(Math.random() * 360);
+    let number2 = Math.floor(Math.random() * 10);
+      videoElem.style.filter = `hue-rotate(${number}deg) blur(${number2}px)`;
+      i++;
+      if (i == 10) {
+        clearInterval(interval);
+      }
+  }, 2000);
+}
+
+async function captureImage(stream) {
+  const mediaTrack = stream.getVideoTracks()[0];
+  console.log(mediaTrack);
+  const captureImg = new ImageCapture(mediaTrack);
+  const photo = await captureImg.takePhoto()
+  console.log(photo)
+  const imgUrl = URL.createObjectURL(photo);
+  console.log(imgUrl);
+  document.querySelector('#photo').src = imgUrl;
+}
+
+
+async function getMedia() {
+  try {
+    stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    const videoElem = document.querySelector('#me');
+    videoElem.srcObject = stream;
+    videoElem.addEventListener('loadedmetadata', () => {
+      videoElem.play();
+      randomiseHue(videoElem);
+    })
+    console.log(stream);
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+getMedia();
+
+document.querySelector('#addImage').addEventListener('click', event => {
+    //document.querySelector('.shakespeare').classList.toggle('hide');
+    captureImage(stream);
+})
 
 function registrateServiceWorker() {
   if ('serviceWorker' in navigator) {
